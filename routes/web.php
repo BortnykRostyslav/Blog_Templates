9257c186105b22377fd0c966c3297a9de787fc14
Bortnyk Rostyslav <bortnikrostislav370@gmail.com>
2024-09-05 16:06:06 +0300

    Add Personal

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-use App\Http\Controllers\Main\IndexController;
-use App\Http\Controllers\Admin\Category;
-use App\Http\Controllers\Admin\User;
-use App\Http\Controllers\Admin\Tag;
-use App\Http\Controllers\Admin\Post;
-use App\Http\Controllers\Admin\Main;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,53 +13,60 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
-    Route::get('/', [IndexController::class, '__invoke']);
+Route::get('/', [App\Http\Controllers\Main\IndexController::class, '__invoke']);
+
+Route::group(['prefix' => 'personal', 'middleware' => ['auth', 'verified']], function () {
+    Route::get('/', [App\Http\Controllers\Personal\Main\IndexController::class, '__invoke'])->name('personal.main.index');
+
+    Route::group(['prefix' => 'liked'], function () {
+        Route::get('/', [App\Http\Controllers\Personal\Liked\IndexController::class, '__invoke'])->name('personal.liked.index');
+    });
+    Route::group(['prefix' => 'comments'], function () {
+        Route::get('/', [App\Http\Controllers\Personal\Comment\IndexController::class, '__invoke'])->name('personal.comment.index');
+    });
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth','admin', 'verified']], function () {
-    Route::group(['namespace' => 'Main'], function () {
-        Route::get('/', [Main\IndexController::class, '__invoke'])->name('admin.main.index');
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'admin', 'verified']], function () {
+    Route::get('/', [App\Http\Controllers\Admin\Main\IndexController::class, '__invoke'])->name('admin.main.index');
+
+    Route::group(['prefix' => 'posts'], function () {
+        Route::get('/', [App\Http\Controllers\Admin\Post\IndexController::class, '__invoke'])->name('admin.post.index');
+        Route::get('/create', [App\Http\Controllers\Admin\Post\CreateController::class, '__invoke'])->name('admin.post.create');
+        Route::post('/', [App\Http\Controllers\Admin\Post\StoreController::class, '__invoke'])->name('admin.post.store');
+        Route::get('/{post}', [App\Http\Controllers\Admin\Post\ShowController::class, '__invoke'])->name('admin.post.show');
+        Route::get('/{post}/edit', [App\Http\Controllers\Admin\Post\EditController::class, '__invoke'])->name('admin.post.edit');
+        Route::patch('/{post}', [App\Http\Controllers\Admin\Post\UpdateController::class, '__invoke'])->name('admin.post.update');
+        Route::delete('/{post}', [App\Http\Controllers\Admin\Post\DeleteController::class, '__invoke'])->name('admin.post.delete');
     });
 
-    Route::group(['namespace' => 'Post', 'prefix' => 'posts'], function () {
-        Route::get('/', [Post\IndexController::class, '__invoke'])->name('admin.post.index');
-        Route::get('/create', [Post\CreateController::class, '__invoke'])->name('admin.post.create');
-        Route::post('/', [Post\StoreController::class, '__invoke'])->name('admin.post.store');
-        Route::get('/{post}', [Post\ShowController::class, '__invoke'])->name('admin.post.show');
-        Route::get('/{post}/edit', [Post\EditController::class, '__invoke'])->name('admin.post.edit');
-        Route::patch('/{post}', [Post\UpdateController::class, '__invoke'])->name('admin.post.update');
-        Route::delete('/{post}', [Post\DeleteController::class, '__invoke'])->name('admin.post.delete');
+    Route::group(['prefix' => 'categories'], function () {
+        Route::get('/', [App\Http\Controllers\Admin\Category\IndexController::class, '__invoke'])->name('admin.category.index');
+        Route::get('/create', [App\Http\Controllers\Admin\Category\CreateController::class, '__invoke'])->name('admin.category.create');
+        Route::post('/', [App\Http\Controllers\Admin\Category\StoreController::class, '__invoke'])->name('admin.category.store');
+        Route::get('/{category}', [App\Http\Controllers\Admin\Category\ShowController::class, '__invoke'])->name('admin.category.show');
+        Route::get('/{category}/edit', [App\Http\Controllers\Admin\Category\EditController::class, '__invoke'])->name('admin.category.edit');
+        Route::patch('/{category}', [App\Http\Controllers\Admin\Category\UpdateController::class, '__invoke'])->name('admin.category.update');
+        Route::delete('/{category}', [App\Http\Controllers\Admin\Category\DeleteController::class, '__invoke'])->name('admin.category.delete');
     });
 
-    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
-        Route::get('/', [Category\IndexController::class, '__invoke'])->name('admin.category.index');
-        Route::get('/create', [Category\CreateController::class, '__invoke'])->name('admin.category.create');
-        Route::post('/', [Category\StoreController::class, '__invoke'])->name('admin.category.store');
-        Route::get('/{category}', [Category\ShowController::class, '__invoke'])->name('admin.category.show');
-        Route::get('/{category}/edit', [Category\EditController::class, '__invoke'])->name('admin.category.edit');
-        Route::patch('/{category}', [Category\UpdateController::class, '__invoke'])->name('admin.category.update');
-        Route::delete('/{category}', [Category\DeleteController::class, '__invoke'])->name('admin.category.delete');
+    Route::group(['prefix' => 'tags'], function () {
+        Route::get('/', [App\Http\Controllers\Admin\Tag\IndexController::class, '__invoke'])->name('admin.tag.index');
+        Route::get('/create', [App\Http\Controllers\Admin\Tag\CreateController::class, '__invoke'])->name('admin.tag.create');
+        Route::post('/', [App\Http\Controllers\Admin\Tag\StoreController::class, '__invoke'])->name('admin.tag.store');
+        Route::get('/{tag}', [App\Http\Controllers\Admin\Tag\ShowController::class, '__invoke'])->name('admin.tag.show');
+        Route::get('/{tag}/edit', [App\Http\Controllers\Admin\Tag\EditController::class, '__invoke'])->name('admin.tag.edit');
+        Route::patch('/{tag}', [App\Http\Controllers\Admin\Tag\UpdateController::class, '__invoke'])->name('admin.tag.update');
+        Route::delete('/{tag}', [App\Http\Controllers\Admin\Tag\DeleteController::class, '__invoke'])->name('admin.tag.delete');
     });
 
-    Route::group(['namespace' => 'Tag', 'prefix' => 'tags'], function () {
-        Route::get('/', [Tag\IndexController::class, '__invoke'])->name('admin.tag.index');
-        Route::get('/create', [Tag\CreateController::class, '__invoke'])->name('admin.tag.create');
-        Route::post('/', [Tag\StoreController::class, '__invoke'])->name('admin.tag.store');
-        Route::get('/{tag}', [Tag\ShowController::class, '__invoke'])->name('admin.tag.show');
-        Route::get('/{tag}/edit', [Tag\EditController::class, '__invoke'])->name('admin.tag.edit');
-        Route::patch('/{tag}', [Tag\UpdateController::class, '__invoke'])->name('admin.tag.update');
-        Route::delete('/{tag}', [Tag\DeleteController::class, '__invoke'])->name('admin.tag.delete');
-    });
-
-    Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
-        Route::get('/', [User\IndexController::class, '__invoke'])->name('admin.user.index');
-        Route::get('/create', [User\CreateController::class, '__invoke'])->name('admin.user.create');
-        Route::post('/', [User\StoreController::class, '__invoke'])->name('admin.user.store');
-        Route::get('/{user}', [User\ShowController::class, '__invoke'])->name('admin.user.show');
-        Route::get('/{user}/edit', [User\EditController::class, '__invoke'])->name('admin.user.edit');
-        Route::patch('/{user}', [User\UpdateController::class, '__invoke'])->name('admin.user.update');
-        Route::delete('/{user}', [User\DeleteController::class, '__invoke'])->name('admin.user.delete');
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', [App\Http\Controllers\Admin\User\IndexController::class, '__invoke'])->name('admin.user.index');
+        Route::get('/create', [App\Http\Controllers\Admin\User\CreateController::class, '__invoke'])->name('admin.user.create');
+        Route::post('/', [App\Http\Controllers\Admin\User\StoreController::class, '__invoke'])->name('admin.user.store');
+        Route::get('/{user}', [App\Http\Controllers\Admin\User\ShowController::class, '__invoke'])->name('admin.user.show');
+        Route::get('/{user}/edit', [App\Http\Controllers\Admin\User\EditController::class, '__invoke'])->name('admin.user.edit');
+        Route::patch('/{user}', [App\Http\Controllers\Admin\User\UpdateController::class, '__invoke'])->name('admin.user.update');
+        Route::delete('/{user}', [App\Http\Controllers\Admin\User\DeleteController::class, '__invoke'])->name('admin.user.delete');
     });
 });
 
