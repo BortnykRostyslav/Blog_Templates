b32e8917755268b6aa9cf73213dc08ea2726cbf3
Bortnyk Rostyslav <bortnikrostislav370@gmail.com>
2024-09-09 15:21:23 +0300

    Add function in Models/Post "category" & "likedUsers"

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Main\IndexController::class, '__invoke']);
+Route::get('/', [App\Http\Controllers\Main\IndexController::class, '__invoke'])->name('main.index');
 
 Route::group(['prefix' => 'personal', 'middleware' => ['auth', 'verified']], function () {
     Route::get('/', [App\Http\Controllers\Personal\Main\IndexController::class, '__invoke'])->name('personal.main.index');
